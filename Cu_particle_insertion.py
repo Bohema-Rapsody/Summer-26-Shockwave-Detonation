96215c4080b94cdc5a376c2d47cccf5095b03ddf
Bohema-Rapsody <volkovoyalexander@gmail.com>
2026-07-31 17:36:00 +0100

    Adding Ti analysis + Cu_300nm update

--- Cu_particle_insertion.py
+++ Cu_particle_insertion.py
@@ -1,33 +1,33 @@
 from LAMMPS_data_modules.data_file_adjustments import *
 
 #Main
-particle = read_data("Cu-N2/data/Cu_equilib.data")
-N2_equilib = read_data("N2_shock/data/N2_equilib.data")
+particle = read_data("Ti-N2/data/Ti_equilib_50.moldata")
+N2_shock = read_data("N2_shock/data/shock_Cu_2.moldata")
 
 
 #Conversion modules to ensure compatibility
 particle.swap_types({1:2, 2:1})
-N2_equilib.convert_real_to_metal()
+N2_shock.convert_real_to_metal()
 #unwrap_x(N2_equilib)
 
 #deleting uneeded molecules
 cut_box(particle,0.0,0.0,0.0,80,keep_inside=True)
-cut_box(N2_equilib,1500.0,0.0,0.0,82,keep_inside=False)
+cut_box(N2_shock,17500.0,0.0,0.0,82,keep_inside=False)
 
-translate(particle,1500.0,0,0)
+translate(particle,17500.0,0,0)
 
 #Renumbering of atoms in the Cu-N2 sim
-atom_num = len(N2_equilib.atoms)
-mol_num = len(N2_equilib.build_molecules())
-bond_num = len(N2_equilib.bonds)
+atom_num = len(N2_shock.atoms)
+mol_num = len(N2_shock.build_molecules())
+bond_num = len(N2_shock.bonds)
 
 renumber(particle,
           atom_offset=atom_num,
           molecule_offset=mol_num,
           bond_offset=bond_num)
 
-combined_sim_Cu_N2 = combine(particle,N2_equilib,box_param=N2_equilib.box)
+combined_sim_Cu_N2 = combine(particle,N2_shock,box_param=N2_shock.box)
 
 combined_sim_Cu_N2.consecutive_atm_ID()
 
-write_data(combined_sim_Cu_N2, "Cu-N2/data/Cu_N2_combined.moldata")
+write_data(combined_sim_Cu_N2, "Ti-N2/data/Ti_N2_shock_ready.moldata")
